642cd3a1ca84aeb383310da34246956838feca1c
AlejaReyes <vreyes2@udi.edu.co>
2025-03-15 14:32:53 -0500

    Finalización del footer (y tratando de agregar un extra)

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ window.addEventListener("scroll", function () {
 });
 
 
-
-
 // Aviso de Cookies
 document.addEventListener("DOMContentLoaded", function () {
     const cookieBanner = document.getElementById("cookie-banner");
@@ -33,5 +31,28 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-// footer
-document.getElementById("year").textContent = new Date().getFullYear();
+// Botón de Scroll Imagen secreta
+document.addEventListener("DOMContentLoaded", function () {
+    let fotoSecreta = document.querySelector(".foto_secreta");
+    let timeout;
+
+    window.addEventListener("scroll", function () {
+        let scrollTop = window.scrollY || document.documentElement.scrollTop;
+        let scrollHeight = document.documentElement.scrollHeight;
+        let clientHeight = document.documentElement.clientHeight;
+
+        if (scrollTop + clientHeight >= scrollHeight - 10) {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                fotoSecreta.style.display = "block";
+            }, 1000); // Se mostrará después de 1 segundo
+        } else {
+            clearTimeout(timeout);
+            fotoSecreta.style.display = "none";
+        }
+    });
+});
+
+
+
+
